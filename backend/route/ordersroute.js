b900af845883db40ea94b5ca47cc57530444de09
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-07-23 13:28:05 +0530

    backend done

--- backend/route/ordersroute.js
+++ backend/route/ordersroute.js
@@ -1,10 +1,11 @@
 const express=require('express');
-const { createorder,getallorders,getorderhistoryofuser } = require('../controller/orderscontroller');
+const { createorder,getallorders,getorderhistoryofuser,updateorder} = require('../controller/orderscontroller');
 
 const router=express.Router();
 
 //endpoints
 router.post('/orders',createorder)
 router.get('/orders',getallorders)
-router.get('/orders/:userid',getorderhistoryofuser)
+router.get('/orders/:userid',getorderhistoryofuser);
+router.put('/orders/:id',updateorder);
 module.exports=router
